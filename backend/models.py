85719d2d662805aeecae7ff46586ae3306423a71
iammeghana@proton.me <iammeghana@proton.me>
2025-01-03 23:51:38 +0530

    removing patients

--- backend/models.py
+++ backend/models.py
@@ -3,24 +3,24 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 # Patient Model
-class Patient(db.Model):
-    __tablename__ = 'patients'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    age = db.Column(db.Integer)
+#class Patient(db.Model):
+    #__tablename__ = 'patients'
+    #id = db.Column(db.Integer, primary_key=True)
+    #name = db.Column(db.String(100), nullable=False)
+   # age = db.Column(db.Integer)
     # Define the relationship to the Note model
-    notes = db.relationship('Note', backref='patient', lazy=True)
+   # notes = db.relationship('Note', backref='patient', lazy=True)
     
-    def __repr__(self):
-        return f'<Patient {self.name}>'
+   # def __repr__(self):
+    #    return f'<Patient {self.name}>'
     
-    def to_json(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "age": self.age,
-            "notes": [note.to_json() for note in self.notes]  # Convert associated notes to JSON as well
-        }
+   # def to_json(self):
+      #  return {
+       #     "id": self.id,
+       #     "name": self.name,
+       #     "age": self.age,
+       #     "notes": [note.to_json() for note in self.notes]  # Convert associated notes to JSON as well
+        #}
 
 
 
@@ -30,7 +30,7 @@ class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text, nullable=False)
     timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
-    patient_id = db.Column(db.Integer, db.ForeignKey('patients.id'), nullable=False)
+    #patient_id = db.Column(db.Integer, db.ForeignKey('patients.id'), nullable=False)
     
     def __repr__(self):
         return f'<Note {self.id}>'
@@ -40,7 +40,7 @@ class Note(db.Model):
             "id" : self.id,
             "content" : self.note_text,
             "timestamp" : self.timestamp,
-            "patient_id" : self.patient_id,
+            #"patient_id" : self.patient_id,
             
             
         }
